c236b73469f5f29bf351194c27ee5c4f72325270
NazmulHasanNahin <nahinn75@gmail.com>
2023-11-11 23:38:30 +0600

    cpp

---
+++ Introduction to C++ for DSA/module 1 basic c++/print_output_in_c++.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+int main()
+{
+    //std::cout<<"Hi this is the output";
+    int a=40;
+    long long int b=10;
+    //std::cout<<a;
+    // std::cout<<"my fav num is "<<a;
+    // std::cout<<"my "<<a<<" fav num is";
+    // std::cout<<a<<" "<<b;
+    // std::cout<<a<<"\n"<<b;
+    std::cout<<a<<std::endl<<b;
+
+    return 0;
+}
---
+++ Introduction to C++ for DSA/module 1 basic c++/take_input_in_c++.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    char c;
+    
+    // cout<<"hello";
+    // cin>>a;
+    // cout<<a;
+    cin>>c;
+    cout<<c;
+    return 0;
+}
