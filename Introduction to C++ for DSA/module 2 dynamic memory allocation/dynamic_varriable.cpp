079daf140a9af5ad3881c343f89ce6a76887cd82
NazmulHasanNahin <nahinn75@gmail.com>
2023-11-12 22:04:06 +0600

    memory allocation

---
+++ Introduction to C++ for DSA/module 2 dynamic memory allocation/dynamic_varriable.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    //int x=10;
+    int *a=new int;
+    *a=11;
+    cout<<*a<<endl;
+    float *f=new float;
+    *f=10.40;
+    cout<<fixed<<setprecision(2)<<*f;
+    return 0;
+}
