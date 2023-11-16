a5429e7db51212f845e766c6d4c08c9b75fcbb62
NazmulHasanNahin <nahinn75@gmail.com>
2023-11-16 21:02:38 +0600

    a

--- Introduction to C++ for DSA/module 3.5 pratice day 2/prartice_1.cpp
+++ Introduction to C++ for DSA/module 3.5 pratice day 2/prartice_1.cpp
@@ -11,15 +11,15 @@ public:
     Student(char *name, int roll, char section, int math_mark, int cls)
     {
         strcpy(this->name, name);
-        this->roll;
-        this->secion;
-        this->math_mark;
-        this->cls;
+        this->roll = roll;
+        this->secion = section;
+        this->math_mark = math_mark;
+        this->cls = cls;
     }
 };
 int main()
 {
     Student first("nahin", 672751, 'b', 90, 12);
-    cout << first.roll;
+    cout << first.name;
     return 0;
 }
