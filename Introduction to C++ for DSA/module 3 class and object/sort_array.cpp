3407adf6473fbce04e43c5d867b3642adbfe37eb
NazmulHasanNahin <nahinn75@gmail.com>
2023-11-16 20:42:06 +0600

    1

--- Introduction to C++ for DSA/module 3 class and object/sort_array.cpp
+++ Introduction to C++ for DSA/module 3 class and object/sort_array.cpp
@@ -9,7 +9,8 @@ int main()
     {
         cin >> ar[i];
     }
-    sort(ar,ar+n,greater<int>());
+    // sort(ar,ar+n);   \\acending order    choto theke boro
+    sort(ar,ar+n,greater<int>());   //decending order       boro theke choto
     for (int i = 0; i < n; i++)
     {
         cout << ar[i] << " ";
