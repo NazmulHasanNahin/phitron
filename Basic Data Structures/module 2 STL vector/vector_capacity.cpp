814a4db62c838a31040c4a6303e40d3ffa85dce5
NazmulHasanNahin <nahinn75@gmail.com>
2023-11-29 00:09:14 +0600

    vector caapcity

---
+++ Basic Data Structures/module 2 STL vector/vector_capacity.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector <int> v;
+    // cout<<v.max_size();
+
+    v.push_back(10);
+    cout<<v.capacity();
+    
+
+
+    return 0;
+}
---
+++ Basic Data Structures/module 2 STL vector/vector_initialization.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    // vector<int> v;   //type 1
+    // cout<<v.size();
+
+    // vector<int> v(5);        //type 2
+    // for(int i=0;i<v.size();i++)
+    // {
+    //     cout<<v[i]<<" ";
+    // }
+    // cout<<endl;
+
+
+    // vector<int> v(5,10);        //type 3
+    // for(int i=0;i<v.size();i++)
+    // {
+    //     cout<<v[i]<<" ";
+    // }
+    // cout<<endl;
+
+
+    // vector<int>v1(5,100);
+    // vector<int> v(v1);        //type 4  copying
+    // for(int i=0;i<v.size();i++)
+    // {
+    //     cout<<v[i]<<" ";
+    // }
+    // cout<<endl;
+
+
+    // int a[6]={1,2,3,4,5,6};
+    // vector<int>v(a,a+6);
+    // for(int i=0;i<v.size();i++)
+    // {
+    //     cout<<v[i]<<" ";
+    // }
+    // cout<<endl;
+
+
+    vector<int>v={10,20,30};        //vector initialization
+    for(int i=0;i<v.size();i++)
+    {
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
