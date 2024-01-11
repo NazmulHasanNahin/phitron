1c14668d8183c1f64a7a3ba7eb5f534c015e5212
NazmulHasanNahin <nahinn75@gmail.com>
2024-01-11 16:17:44 +0600

    set

---
+++ Basic Data Structures/module 23 STL priority queue, map and set/stl_map.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    map<string,int>mp;
+    mp.insert({"nahin",69});
+    mp.insert({"rahim",59});
+    mp.insert({"karim",39});
+    cout<<mp["nahin"]<<endl;
+    return 0;
+}
