44d14667531354b2aca6d1d3289cf0b762e985c8
NazmulHasanNahin <nahinn75@gmail.com>
2024-02-15 15:05:54 +0600

    leetcode

---
+++ Introduction to Algorithms/module 9 leetcode prblm solve/number_of_island.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    int n,m;
+    bool vis[305][305];
+    int ans=0;
+    vector<pair<int,int>> d={{0,1},{0,-1},{-1,0},{1,0}};
+    bool valid(int ci,int cj)
+    {
+        if(ci>=0 && ci<n && cj>=0 && cj<m)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    void dfs(int si,int sj,vector<vector<char>>& grid)
+    {
+        vis[si][sj]=true; 
+        for(int i=0;i<4;i++)
+        {
+            int ci=si+d[i].first;
+            int cj=sj+d[i].second;
+            if(valid(ci,cj) && !vis[ci][cj] && grid[ci][cj]=='1')
+            {
+                dfs(ci,cj,grid);
+            }
+        }
+    }
+    int numIslands(vector<vector<char>>& grid) 
+    {
+        memset(vis,false,sizeof(vis));
+        n=grid.size();
+        m=grid[0].size();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(!vis[i][j] && grid[i][j]=='1')
+                {
+                    dfs(i,j,grid);
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+};
+// https://leetcode.com/problems/number-of-islands/
