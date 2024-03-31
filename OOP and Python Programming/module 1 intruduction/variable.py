18ec2336ae9cb91c38c00267a71dc8f050f2a8c6
NazmulHasanNahin <nahinn75@gmail.com>
2024-03-31 21:36:30 +0600

    py

---
+++ OOP and Python Programming/module 1 intruduction/variable.py
@@ -0,0 +1,12 @@
+# age=10                         //int
+# print(age)
+# print(type(age))
+
+
+# name="nahin"                  //string
+# print(type(name))
+
+
+# is_right=True                     //bool
+# print(type(is_right))
+
