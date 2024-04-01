ba2e46f2394231f1f18c5e426672b65ef4dcb15c
NazmulHasanNahin <nahinn75@gmail.com>
2024-04-01 21:25:09 +0600

    py

---
+++ OOP and Python Programming/module 1 intruduction/input_in_python.py
@@ -0,0 +1,10 @@
+ammount=input("the ammount i need: ")
+
+
+
+#by default python e input string hishabe ney 
+
+fist=input("gimme first num: ")
+second=input("gimme second num: ")
+total=fist+second
+print(second)
