568c0a97dd58ec125bb28916be1149e5e73bc76d
NazmulHasanNahin <nahinn75@gmail.com>
2024-04-05 22:14:59 +0600

    py 2

---
+++ OOP and Python Programming/module 3 basic python 2/built_in_modules.py
@@ -0,0 +1,18 @@
+# from math import *
+# # result=floor(5.1)
+# # print(result)
+
+# result=ceil(5.1)
+# print(result)
+
+
+
+####################
+
+
+
+from random import *
+# rnd=randint(0,2)
+# print(rnd)
+
+print(choice(["head","tail","none"]))
---
+++ OOP and Python Programming/module 3 basic python 2/string_.py
@@ -0,0 +1,6 @@
+name ="nahin"
+print(name.upper())
+if "N" in name:
+    print("ase")        #to cheak 
+else:
+    print("Nai")
