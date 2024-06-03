4c52d52cf75fcb30f53e31b2c7d66edb70afc6b9
NazmulHasanNahin <nahinn75@gmail.com>
2024-06-03 19:36:55 +0600

    js

--- Software Development Project/module 5 javascript intro/pratice.js
+++ Software Development Project/module 5 javascript intro/pratice.js
@@ -1,3 +1,22 @@
-var test="Nahin";
+// var test="Nahin";
 
-console.log(test);
+// console.log(test);
+
+// var frnd=["nahin","franklin","trevor"];
+
+// for(var i=0;i<frnd.length;i++)
+//     {
+//         console.log(frnd[i]);
+//     }
+
+// var namme="nahin";
+// console.log(namme);
+
+function sum(num1,num2) 
+    {
+        var result=num1+num2;
+        console.log(result);
+    }
+sum(10,20)
+
+
