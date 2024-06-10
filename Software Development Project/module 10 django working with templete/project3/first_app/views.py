93b3ea2160be639a67e138db2ac381a34e2e4861
NazmulHasanNahin <nahinn75@gmail.com>
2024-06-10 20:54:10 +0600

    django filter testting

--- Software Development Project/module 10 django working with templete/project3/first_app/views.py
+++ Software Development Project/module 10 django working with templete/project3/first_app/views.py
@@ -4,5 +4,21 @@ from django.shortcuts import render
 
 
 def home(request):
-    d={"author" : "rahim" , "age" : 20}
+    d={"author" : "rahim" , "age" : 29 ,"list":["python","is","best"],"courses":[
+        {
+            "id":1,
+            "name" : "python",
+            "fee" : 6500,
+        },
+        {
+            "id":2,
+            "name" : "django",
+            "fee" : 5500,
+        },
+        {
+            "id":3,
+            "name" : "c",
+            "fee" : 4500,
+        }
+    ]}
     return render(request,"home.html",d)
