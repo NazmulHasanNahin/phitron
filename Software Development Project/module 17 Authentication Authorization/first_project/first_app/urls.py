218def2a9ab9e6ae072b517cb6115309c6de4c5a
NazmulHasanNahin <nahinn75@gmail.com>
2024-06-29 13:30:48 +0600

    login logout

--- Software Development Project/module 17 Authentication Authorization/first_project/first_app/urls.py
+++ Software Development Project/module 17 Authentication Authorization/first_project/first_app/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path,include
-from first_app.views import home
+from first_app.views import *
 
 urlpatterns = [
     path("",home,name="homepage"),
+    path("singup/",singup,name="singup"),
+    path("login/",user_login,name="login"),
+    path("profile/",profile,name="profile"),
+    path("logout/",user_logout,name="logout"),
     
 ]
--- Software Development Project/module 17 Authentication Authorization/first_project/first_app/views.py
+++ Software Development Project/module 17 Authentication Authorization/first_project/first_app/views.py
@@ -1,14 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from first_app.forms import RegisterForm
 from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate,login,logout
 # Create your views here.
 
 def home(rq):
-    if rq.method == "POST":
-        form=RegisterForm(rq.POST)
-        if form.is_valid():
-            messages.success(rq,"ACC Created ")
-            form.save()
+    return render(rq,"home.html")
+
+def singup(rq):
+    if not rq.user.is_authenticated:
+        if rq.method == "POST":
+            form=RegisterForm(rq.POST)
+            if form.is_valid():
+                messages.success(rq,"ACC Created ")
+                form.save()
+        else:
+            form=RegisterForm()
+        return render(rq,"singup.html",{"form":form})
     else:
-        form=RegisterForm()
-    return render(rq,"home.html",{"form":form})
+        return redirect("profile")
+
+def user_login(rq):
+    if not rq.user.is_authenticated:
+        if rq.method=="POST":
+            form=AuthenticationForm(request=rq,data=rq.POST)
+            if form.is_valid():
+                name=form.cleaned_data["username"]
+                userpass = form.cleaned_data["password"]
+                user=authenticate(username=name,password=userpass)
+                if user is not None:
+                    login(rq,user)
+                    return redirect("profile")
+        else:
+            form=AuthenticationForm()
+        return render(rq,"login.html",{"form":form})
+    else:
+        return redirect("profile")
+    
+def profile(rq):
+    if rq.user.is_authenticated:
+        return render(rq,"profile.html",{"user":rq.user})
+    else:
+        return redirect("login")
+
+
+def user_logout(rq):
+    logout(rq)
+    return redirect("login")
