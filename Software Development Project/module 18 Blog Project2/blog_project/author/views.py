b9ee98059ff5ced2889ac46900e38f05cef7b54f
NazmulHasanNahin <nahinn75@gmail.com>
2024-07-01 13:06:54 +0600

    bl project 2

--- Software Development Project/module 18 Blog Project2/blog_project/author/views.py
+++ Software Development Project/module 18 Blog Project2/blog_project/author/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from author.forms import *
-from django.contrib.auth.forms import AuthenticationForm,PasswordChangeForm
-from django.contrib.auth import authenticate,login,logout,update_session_auth_hash
+from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from posts.models import *
+
 # Create your views here.
 
 # def add_authors(rq):
@@ -13,44 +15,45 @@ from django.contrib.auth.decorators import login_required
 #             author_form.save()
 #             return redirect("add_authors")
 #     else:
-#         author_form=AuthorForm() 
+#         author_form=AuthorForm()
 #     return render(rq,"add_author.html",{"form": author_form})
 
 
-
 def register(rq):
-    if rq.method=="POST":
-        register_form=RegistrationForm(rq.POST)
+    if rq.method == "POST":
+        register_form = RegistrationForm(rq.POST)
         if register_form.is_valid():
             register_form.save()
-            messages.success(rq,"Account Registred Successfully")
+            messages.success(rq, "Account Registred Successfully")
             return redirect("register")
     else:
-        register_form=RegistrationForm() 
-    return render(rq,"register.html",{"form": register_form,"type":"Register"})
+        register_form = RegistrationForm()
+    return render(rq, "register.html", {"form": register_form, "type": "Register"})
 
 
 def user_login(rq):
-    if rq.method=="POST":
-        form=AuthenticationForm(rq,rq.POST)
+    if rq.method == "POST":
+        form = AuthenticationForm(rq, rq.POST)
         if form.is_valid():
-            user_name=form.cleaned_data["username"]
-            user_pass=form.cleaned_data["password"]
-            user=authenticate(username=user_name,password=user_pass)
+            user_name = form.cleaned_data["username"]
+            user_pass = form.cleaned_data["password"]
+            user = authenticate(username=user_name, password=user_pass)
             if user is not None:
-                login(rq,user)
-                messages.success(rq,"Login Successfully")
+                login(rq, user)
+                messages.success(rq, "Login Successfully")
                 return redirect("profile")
             else:
-                messages.warning(rq,"Login info Incorrect")
+                messages.warning(rq, "Login info Incorrect")
                 return redirect("register")
     else:
-        form=AuthenticationForm()
-        return render(rq,"register.html",{"form": form,"type":"Login"})
-    
-@login_required    
+        form = AuthenticationForm()
+        return render(rq, "register.html", {"form": form, "type": "Login"})
+
+
+@login_required
 def profile(rq):
-    return render(rq,"profile.html")
+    data = Posts.objects.filter(author=rq.user)
+    return render(rq,"profile.html",{"data":data})
 
 
 @login_required    
--- Software Development Project/module 18 Blog Project2/blog_project/posts/views.py
+++ Software Development Project/module 18 Blog Project2/blog_project/posts/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, redirect
 from . import forms
 from . import models
+from posts.models import *
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 
-
+@login_required
 def add_post(rq):
     if rq.method == "POST":
         post_form=forms.PostForm(rq.POST)
@@ -18,18 +21,21 @@ def add_post(rq):
 
 
 
-
+@login_required
 def edit_post(rq, id):
     post = forms.Posts.objects.get(pk=id) 
     post_form = forms.PostForm(instance=post)
     if rq.method == 'POST':
         post_form = forms.PostForm(rq.POST, instance=post)
         if post_form.is_valid():
+            post_form.instance.author=rq.user
             post_form.save()
             return redirect('homepage')
     
     return render(rq, 'add_post.html', {'form' : post_form})
 
+
+@login_required
 def delete_post(rq,id):
     post = forms.Posts.objects.get(pk=id) 
     post.delete()
