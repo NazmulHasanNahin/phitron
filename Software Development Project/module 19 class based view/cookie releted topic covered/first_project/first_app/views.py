ca782dfd6b32e129237b1280805a8a7947604544
NazmulHasanNahin <nahinn75@gmail.com>
2024-07-03 21:47:15 +0600

    cookie session

---
+++ Software Development Project/module 19 class based view/cookie releted topic covered/first_project/first_app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    response=render(request,"home.html")
+    response.set_cookie("name","nahin")
+    return response
