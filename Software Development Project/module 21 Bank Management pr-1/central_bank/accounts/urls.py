caf45cf9db883f68f438485015f635da06fdf103
NazmulHasanNahin <nahinn75@gmail.com>
2024-07-07 21:34:47 +0600

    1st part end

--- Software Development Project/module 21 Bank Management pr-1/central_bank/accounts/urls.py
+++ Software Development Project/module 21 Bank Management pr-1/central_bank/accounts/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from accounts.views import *
 
 urlpatterns = [
-    path("register/",UserRegistrationView.as_view(),name="register"),
-    
+    path("register/", UserRegistrationView.as_view(), name="register"),
+    path("login/", UserLoginView.as_view(), name="login"),
+    path("logout/", UserLogoutView.as_view(), name="logout"),
+    path("profile/", UserBankAccountUpdateView.as_view(), name="profile"),
 ]
--- Software Development Project/module 21 Bank Management pr-1/central_bank/accounts/views.py
+++ Software Development Project/module 21 Bank Management pr-1/central_bank/accounts/views.py
@@ -1,9 +1,11 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.views.generic import FormView
 from accounts.forms import *
+from django.views import View
 from django.urls import reverse_lazy
 from django.contrib.auth import login,logout
+from django.contrib.auth.views import LoginView,LogoutView
 
 # Create your views here.
 
@@ -11,11 +13,34 @@ from django.contrib.auth import login,logout
 class UserRegistrationView(FormView):
     template_name="accounts/user_registration.html"
     form_class=UserRegistrationForm
-    success_url = reverse_lazy("register")
+    success_url = reverse_lazy("login")
     
     def form_valid(self, form):
-        user=form.save()
-        login(self.request,user)
+        form.save(commit=True)
         return super().form_valid(form)
     
+    
+class UserLoginView(LoginView):
+    template_name="accounts/user_login.html"
+    def get_success_url(self):
+        return reverse_lazy("home")
+    
+class UserLogoutView(LogoutView):
+    def get_success_url(self):
+        if self.request.user.is_authenticated:
+            logout(self.request)
+        return reverse_lazy("home")
+    
+class UserBankAccountUpdateView(View):
+    template_name ='accounts/profile.html'
+    def get(self, request):
+        form = UserUpdateForm(instance=request.user)
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = UserUpdateForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+        return render(request, self.template_name, {'form': form})
     
