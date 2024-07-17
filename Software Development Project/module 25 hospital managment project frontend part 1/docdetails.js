494897e6c1e8c38ca04771bccf5c2a7c75ea7543
NazmulHasanNahin <nahinn75@gmail.com>
2024-07-17 16:55:26 +0600

    login reg page added

--- Software Development Project/module 25 hospital managment project frontend part 1/docdetails.js
+++ Software Development Project/module 25 hospital managment project frontend part 1/docdetails.js
@@ -1,37 +1,36 @@
 const getparams = () => {
     const param = new URLSearchParams(window.location.search).get("doctorId");
+    loadTime(param)
     fetch(`https://testing-8az5.onrender.com/doctor/list/${param}`)
         .then(res => res.json())
         .then((data) => displayDeteails(data));
 
 
     fetch(`https://testing-8az5.onrender.com/doctor/review/?doctor_id=${param}`)
-    .then((res)=>res.json())
-    .then((data)=>{
-        doctordisplayReview(data);
-    });
+        .then((res) => res.json())
+        .then((data) => {
+            doctordisplayReview(data);
+        });
 
 };
 const displayDeteails = (doctor) => {
     const parent = document.getElementById("doc-details");
-    const div=document.createElement("div");
+    const div = document.createElement("div");
     div.classList.add("doc-details-container");
-    div.innerHTML=`
+    div.innerHTML = `
     <div class="doc-img">
             <img src=${doctor.image} alt="">
         </div>
     <div class="mx-5 doc-info">
         <h1 class="doc-name fw-bolder">${doctor.full_name}</h1>
-        ${
-            doctor.specialization.map((item)=>{
-                return `<button class="doc-details-btn">${item}</button>`
-            })
+        ${doctor.specialization.map((item) => {
+        return `<button class="doc-details-btn">${item}</button>`
+    })
         }
         </br>
-        ${
-            doctor.designation.map((item)=>{
-                return `<h2 class="doc-details-btn-desig fw-bold">${item}</h2>`
-            })
+        ${doctor.designation.map((item) => {
+            return `<h2 class="doc-details-btn-desig fw-bold">${item}</h2>`
+        })
         }
         <p class="des-dc w-50">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi hic dignissimos asperiores
             placeat
@@ -43,7 +42,7 @@ const displayDeteails = (doctor) => {
     </div>
     `;
     parent.appendChild(div);
-}
+};
 
 
 const doctordisplayReview = (reviews) => {
@@ -60,6 +59,52 @@ const doctordisplayReview = (reviews) => {
         `;
         parent.appendChild(div)
     })
-}
+};
+
+const loadTime = (id) => {
+    fetch(`https://testing-8az5.onrender.com/doctor/availabletime/?doctor_id=${id}`)
+        .then(res => res.json())
+        .then((data) => {
+
+            data.forEach((item) => {
+                const parent = document.getElementById("time-container");
+                const option = document.createElement("option");
+                option.value = item.id;
+                option.innerText = item.name;
+                parent.appendChild(option);
+            })
+
+        })
+};
+
+const handleAppoinment = () => {
+    const param = new URLSearchParams(window.location.search).get("doctorId");
+    const status = document.getElementsByName("status");
+    const selected = Array.from(status).find((button) => button.checked);
+    const symtom = document.getElementById("symtom").value;
+    const time = document.getElementById("time-container");
+    const selectedTime = time.options[time.selectedIndex];
+    const info = {
+        appointment_type: selected.value,
+        appointment_status: "Pending",
+        time: selectedTime.value,
+        symptom: symtom,
+        cancel: false,
+        patient: 1,
+        doctor: param,
+    };
+    // console.log(info);
+
+    fetch("https://testing-8az5.onrender.com/appointment/", {
+        method: "POST",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify(info),
+    })
+        .then(res => res.json())
+        .then((data) => {
+            console.log(data);
+        })
+};
 
 getparams();
+loadTime();
