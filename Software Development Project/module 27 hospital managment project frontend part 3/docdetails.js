f4eb7494dd723e0e745f175d51033e746fd22354
NazmulHasanNahin <nahinn75@gmail.com>
2024-07-27 17:18:15 +0600

    frontend completed

--- Software Development Project/module 27 hospital managment project frontend part 3/docdetails.js
+++ Software Development Project/module 27 hospital managment project frontend part 3/docdetails.js
@@ -1,50 +1,18 @@
 const getparams = () => {
     const param = new URLSearchParams(window.location.search).get("doctorId");
-    if (param) {
-        loadTime(param);
-        fetchDoctorDetails(param);
-        fetchDoctorReviews(param);
-    }
-};
-
-const fetchDoctorDetails = (param) => {
+    loadTime(param);
     fetch(`https://testing-8az5.onrender.com/doctor/list/${param}`)
-        .then(res => res.json())
-        .then(displayDetails)
-        .catch(error => console.error('Error fetching doctor details:', error));
-};
+        .then((res) => res.json())
+        .then((data) => displayDetails(data));
 
-const displayDetails = (doctor) => {
-    const parent = document.getElementById("doc-details");
-    const div = document.createElement("div");
-    div.classList.add("doc-details-container");
-    div.innerHTML = `
-        <div class="doc-img">
-            <img src=${doctor.image} alt="Doctor Image">
-        </div>
-        <div class="mx-5 doc-info">
-            <h1 class="doc-name fw-bolder">${doctor.full_name}</h1>
-            ${doctor.specialization.map(item => `<button class="doc-details-btn">${item}</button>`).join('')}
-            <br>
-            ${doctor.designation.map(item => `<h2 class="doc-details-btn-desig fw-bold">${item}</h2>`).join('')}
-            <p class="des-dc w-50">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi hic dignissimos asperiores placeat perferendis nostrum distinctio quibusdam assumenda quasi eos.</p>
-            <h4 class="fee-amnt">Fees: ${doctor.fee}</h4>
-            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Take Appointment</button>
-        </div>
-    `;
-    parent.appendChild(div);
-};
-
-const fetchDoctorReviews = (param) => {
     fetch(`https://testing-8az5.onrender.com/doctor/review/?doctor_id=${param}`)
-        .then(res => res.json())
-        .then(doctordisplayReview)
-        .catch(error => console.error('Error fetching doctor reviews:', error));
+        .then((res) => res.json())
+        .then((data) => doctorReview(data));
 };
 
-const doctordisplayReview = (reviews) => {
-    const parent = document.getElementById("doc-details-review");
+const doctorReview = (reviews) => {
     reviews.forEach((review) => {
+        const parent = document.getElementById("doc-details-review");
         const div = document.createElement("div");
         div.classList.add("review-card");
         div.innerHTML = `
@@ -57,28 +25,65 @@ const doctordisplayReview = (reviews) => {
     });
 };
 
+const displayDetails = (doctor) => {
+    console.log(doctor);
+    const parent = document.getElementById("doc-details");
+    const div = document.createElement("div");
+    div.classList.add("doc-details-container");
+    div.innerHTML = `
+      <div class="doc-img">
+      <img src=${doctor.image} alt="" />
+    </div>
+    <div class="doc-info">
+      <h1>${doctor.full_name} </h1>
+      ${doctor.specialization.map((item) => {
+        return `<button class="doc-detail-btn">${item}</button>`;
+    })}
+      ${doctor.designation.map((item) => {
+        return `<h4 >${item}</h4>`;
+    })}
+  
+      <p class="w-50">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Et quibusdam
+        quis excepturi tempore. Eius, qui!
+      </p>
+  
+      <h4>Fees: ${doctor.fee} BDT</h4>
+      <button
+      type="button"
+      class="btn btn-primary"
+      data-bs-toggle="modal"
+      data-bs-target="#exampleModal"
+    >
+     Take Appointment
+    </button>
+    </div>
+      `;
+    parent.appendChild(div);
+};
+
 const loadTime = (id) => {
     fetch(
-      `https://testing-8az5.onrender.com/doctor/availabletime/?doctor_id=${id}`
+        `https://testing-8az5.onrender.com/doctor/availabletime/?doctor_id=${id}`
     )
-      .then((res) => res.json())
-      .then((data) => {
-        data.forEach((item) => {
-          const parent = document.getElementById("time-container");
-          const option = document.createElement("option");
-          option.value = item.id;
-          option.innerText = item.name;
-          parent.appendChild(option);
+        .then((res) => res.json())
+        .then((data) => {
+            data.forEach((item) => {
+                const parent = document.getElementById("time-container");
+                const option = document.createElement("option");
+                option.value = item.id;
+                option.innerText = item.name;
+                parent.appendChild(option);
+            });
+            console.log(data);
         });
-        console.log(data);
-      });
-  };
+};
 
 const handleAppointment = () => {
     const param = new URLSearchParams(window.location.search).get("doctorId");
     const status = document.getElementsByName("status");
     const selected = Array.from(status).find((button) => button.checked);
-    const symptom = document.getElementById("symptom");
+    const symptom = document.getElementById("symptom").value;
     const time = document.getElementById("time-container");
     const selectedTime = time.options[time.selectedIndex];
     const patient_id = localStorage.getItem("patient_id");
@@ -106,8 +111,7 @@ const handleAppointment = () => {
         });
 };
 
-
-const loadPatientID = () => {
+const loadPatientId = () => {
     const user_id = localStorage.getItem("user_id");
 
     fetch(`https://testing-8az5.onrender.com/patient/list/?user_id=${user_id}`)
@@ -117,20 +121,6 @@ const loadPatientID = () => {
         });
 };
 
-const loadAllAppointments = () => {
-    const patient_id = localStorage.getItem("patient_id");
-    if (patient_id) {
-        fetch(`https://testing-8az5.onrender.com/appointment/?patient_id=${patient_id}`)
-            .then(res => res.json())
-            .then((data) => {
-                console.log('Appointments:', data);
-            })
-            .catch(error => console.error('Error fetching appointments:', error));
-    } else {
-        console.error('No patient ID found in local storage');
-    }
-};
-
-loadPatientID();
+loadPatientId();
 getparams();
-loadAllAppointments();
+loadTime();
