4eddfc6e46e31ec28dca8f9ba0cb45c97b06ed56
NazmulHasanNahin <nahinn75@gmail.com>
2024-08-14 21:23:35 +0600

    corf

--- Software Development Project/Final Assingment Job Board Project backend/workwave/workwave/settings.py
+++ Software Development Project/Final Assingment Job Board Project backend/workwave/workwave/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = 'django-insecure-=uak7caj+7i^&ij&v35on@@tf=q)%hz2$tc5lqaygc&hfxhk57
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["http://127.0.0.1:5500","*"]
 
 
 # Application definition
@@ -59,6 +59,12 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:5500",
+    "http://127.0.0.1:7000",
+    "http://localhost:5500",
+]
 
 ROOT_URLCONF = 'workwave.urls'
 
