6118a4bcc50186f75bf7d6c44a0e46ac43b9eecc
NazmulHasanNahin <nahinn75@gmail.com>
2024-08-21 14:33:36 +0600

    job serializer fix

--- Software Development Project/Final Assingment Job Board Project backend/workwave/jobs/serializers.py
+++ Software Development Project/Final Assingment Job Board Project backend/workwave/jobs/serializers.py
@@ -5,7 +5,7 @@ from .models import Job, JobCategory
 class JobSerializer(serializers.ModelSerializer):
     class Meta:
         model = Job
-        fields = ['title', 'description', 'category', 'location', 'experience', 'employee_type', 'position', 'offer_salary', 'responsibilities', 'qualifications', 'skills_experience']
+        fields = ["id",'title', 'description', 'category', 'location', 'experience', 'employee_type', 'position', 'offer_salary', 'responsibilities', 'qualifications', 'skills_experience',"date_posted"]
         
 
 
