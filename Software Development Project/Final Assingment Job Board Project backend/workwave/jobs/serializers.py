a4b8b7736afbacc51af810505f9fb49d0408193b
NazmulHasanNahin <nahinn75@gmail.com>
2024-08-26 22:28:59 +0600

    edit update delete

--- Software Development Project/Final Assingment Job Board Project backend/workwave/jobs/serializers.py
+++ Software Development Project/Final Assingment Job Board Project backend/workwave/jobs/serializers.py
@@ -5,7 +5,9 @@ from .models import Job, JobCategory
 class JobSerializer(serializers.ModelSerializer):
     class Meta:
         model = Job
-        fields = ["id",'title', 'description', 'category', 'location', 'experience', 'employee_type', 'position', 'offer_salary', 'responsibilities', 'qualifications', 'skills_experience',"date_posted"]
+        fields = ["id",'title', 'description', 'category',
+                  'location', 'experience', 'employee_type', 'position', 'offer_salary'
+                  , 'responsibilities', 'qualifications', 'skills_experience',"date_posted"]
         
 
 
