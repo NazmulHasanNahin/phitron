a9739b71977b0c33df9ddb17922988bd98b4dcca
NazmulHasanNahin <nahinn75@gmail.com>
2025-01-05 22:24:12 +0600

    dd

--- React Learning/Intruduction to simple react/2.39-8 Thinking in React way/learning_react/src/App.jsx
+++ React Learning/Intruduction to simple react/2.39-8 Thinking in React way/learning_react/src/App.jsx
@@ -1,16 +1,15 @@
-import { useState } from 'react'
 import './App.css'
 
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      
       <h1>react core concept</h1>
-      
+      <Header></Header>
     </>
   )
 }
+
 
 export default App
